aad93490093296dcff209f1e41a60674932f8250
martindonelli <martinnicolasd@gmail.com>
2023-05-29 03:00:55 -0300

    carga de seccion  api

--- script.js
+++ script.js
@@ -27,3 +27,34 @@ function seleccionar(){
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
+
+const boton = document.querySelector('#boton');
+const nombre = document.querySelector('#nombre');
+const apellido = document.querySelector('#apellido');
+const correo = document.querySelector('#correo');
+const colaborando = document.querySelector('#colaborando');
+const nacionalidad = document.querySelector('#nacionalidad');
+const oriundo = document.querySelector('#oriundo');
+const foto = document.querySelector('#foto');
+
+const generarUsuario = async () => {
+    //consultar a la API
+    const url = 'https://randomuser.me/api/';
+    const respuesta = await fetch(url);
+    const { results } = await respuesta.json();
+    const datos = results[0];
+
+    foto.src = datos.picture.large;
+    nombre.textContent = datos.name.first;
+    apellido.textContent = datos.name.last;
+    correo.textContent = datos.email;
+    colaborando.textContent = datos.registered.date;
+    nacionalidad.textContent = datos.location.country;
+    oriundo.textContent = datos.location.state;
+    console.log(results[0])
+}
+
+
+// evento para que el DOM inicie con un chef 
+document.addEventListener('DOMContentLoaded', generarUsuario);
+boton.addEventListener('click', generarUsuario);
